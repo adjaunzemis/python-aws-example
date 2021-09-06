6e9f2d8a034b0afeacb56c0a661f6b897f6e8a74
Andris Jaunzemis <adjaunzemis@gmail.com>
2021-09-06 15:26:12 -0400

    Added more routes for testing

--- application.py
+++ application.py
@@ -4,4 +4,16 @@ application = Flask(__name__)
 
 @application.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello world! WAATech is on the rise!'
+
+@application.route('/andris')
+def hello_andris():
+    return 'Hello Andris, we\'ve been waiting for you...'
+
+@application.route('/alex')
+def hello_alex():
+    return 'Hello Alex, we\'ve been waiting for you...'
+
+@application.route('/wes')
+def hello_wes():
+    return 'Hello Wes, we\'ve been waiting for you...'
