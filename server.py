91b8d49c70bc569fb91ed45d0fd2463ee3f45200
Andris Jaunzemis <adjaunzemis@gmail.com>
2021-09-09 21:26:22 -0400

    Implemented test REST API
    Updated package requirements and adjusted AWS configuration.

--- server.py
+++ server.py
@@ -1,19 +1,30 @@
 from flask import Flask
+from flask_restx import Resource, Api, reqparse
 
-testApp = Flask(__name__)
+app = Flask(__name__)
+api = Api(app=app)
 
-@testApp.route('/')
-def hello_world():
-    return 'Hello world! WAATech is on the rise!'
+@api.route('/api/profit')
+class Profit(Resource):
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('person', type=str, default=None, required=True, help='Person to profit')
+        parser.add_argument('amount', type=float, default=0, required=False, help='Profit amount')
+        args = parser.parse_args()
 
-@testApp.route('/andris')
+        return f"{args['person']} profits ${args['amount']:.2f}, hooray!"
+
+@app.route('/andris')
 def hello_andris():
     return 'Hello Andris, we\'ve been waiting for you...'
 
-@testApp.route('/alex')
+@app.route('/alex')
 def hello_alex():
     return 'Hello Alex, we\'ve been waiting for you...'
 
-@testApp.route('/wes')
+@app.route('/wes')
 def hello_wes():
     return 'Hello Wes, we\'ve been waiting for you...'
+
+if __name__ == "__main__":
+    app.run(debug=True)
