1e30fffb43bb7aac3521ba18fdc59b90fd5fbaa0
Andris Jaunzemis <adjaunzemis@gmail.com>
2021-09-09 20:44:52 -0400

    Testing change to Flask instance variable name

--- application.py
+++ application.py
@@ -1,19 +1,19 @@
 from flask import Flask
 
-application = Flask(__name__)
+testApp = Flask(__name__)
 
-@application.route('/')
+@testApp.route('/')
 def hello_world():
     return 'Hello world! WAATech is on the rise!'
 
-@application.route('/andris')
+@testApp.route('/andris')
 def hello_andris():
     return 'Hello Andris, we\'ve been waiting for you...'
 
-@application.route('/alex')
+@testApp.route('/alex')
 def hello_alex():
     return 'Hello Alex, we\'ve been waiting for you...'
 
-@application.route('/wes')
+@testApp.route('/wes')
 def hello_wes():
     return 'Hello Wes, we\'ve been waiting for you...'
