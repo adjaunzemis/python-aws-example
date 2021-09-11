3a7b9c737bed9885f612b0f5eb0564f55ef587b6
Andris Jaunzemis <adjaunzemis@gmail.com>
2021-09-11 08:48:58 -0400

    Partial switch of flask implementation to use flask-smorest

--- server.py
+++ server.py
@@ -1,48 +1,58 @@
-from flask import Flask, request
-from flask_restx import Resource, Api, Namespace
-from marshmallow import Schema, fields
-from marshmallow.validate import Length
+from flask import Flask
+from flask.views import MethodView
+from flask_smorest import Api, Blueprint, abort
+import marshmallow as ma
 
 from dataclasses import dataclass
 
 @dataclass
-class Profit():
+class Profit:
+    id: int
     name: str
-    amount: float = 0.0
+    amount: float
     
-class ProfitSchema(Schema):
-    name = fields.String(required=True, validate=Length(max=10))
-    amount = fields.Float(required=False, default=0.0, missing=0.0)
+class ProfitSchema(ma.Schema):
+    id = ma.fields.Integer()
+    name = ma.fields.String()
+    amount = ma.fields.Float()
 
-profitAPI = Namespace("Profit Machine", description="A profit making machine")
+class ProfitQueryArgsSchema(ma.Schema):
+    name = ma.fields.String()
+    amount = ma.fields.Float()
 
-profitsSchema = ProfitSchema(many=True)
-profitSchema = ProfitSchema()
-@profitAPI.route("/")
-class ProfitResource(Resource):
-    def get(self):
-        """
-        Some documentation for ProfitResource get function goes here.
-        """
-        errors = profitSchema.validate(request.args)
-        if errors:
-            return { 'errors': str(errors) }, 400
-        values = profitSchema.load(request.args)
-        return { 'data': profitSchema.dump(Profit(name=values['name'], amount=values['amount'])) }, 200
-     
-app = Flask(__name__)
-
-mainAPI = Api(
-    app = app,
-    title = "Test API",
-    version = "1.0.0",
-    description = "A super cool test API, complete with dope auto-documentation!"
+blp = Blueprint(
+    "profits", "profits", url_prefix="/profits", description="Operations on profits"
 )
-mainAPI.add_namespace(profitAPI, path="/api/profit")
 
-@app.route("/andris")
-def hello_andris():
-    return "Hello Andris, we've been waiting for you..."
+@blp.route("/")
+class Profits(MethodView):
+
+    @blp.arguments(ProfitQueryArgsSchema, location="query")
+    @blp.response(200, ProfitSchema)
+    def get(self, args):
+        """ Profit amount for a specific person """
+        return Profit(1, "AJ", 123.456)
+
+class Config:
+    API_TITLE = "Test API"
+    API_VERSION = "1.0.0"
+    OPENAPI_VERSION = "3.0.2"
+    OPENAPI_URL_PREFIX = '/doc'
+    OPENAPI_REDOC_PATH = '/redoc'
+    OPENAPI_SWAGGER_UI_PATH = '/swagger'
+    OPENAPI_SWAGGER_UI_VERSION = '3.24.2'
+# app.config['API_DESCRIPTION'] = "A super cool test API, complete with dope auto-documentation!"
+    
+app = Flask(__name__)
+app.config.from_object(Config)
+
+api = Api(app = app)
+
+api.register_blueprint(blp)
+
+# @app.route("/andris")
+# def hello_andris():
+#     return "Hello Andris, we've been waiting for you..."
 
 if __name__ == "__main__":
     app.run(debug=True)
